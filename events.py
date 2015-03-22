ac3846a9eb00c52a57290e0e03e1f57cbb222fcf
Ilia Z <ilia.z.freelance@outlook.com>
2015-03-22 13:02:38 +0500

    Special events plotting is underway

--- events.py
+++ events.py
@@ -1,57 +1,114 @@
-import datetime
+import re
+import argparse
+from datetime import datetime, timedelta
+from operator import methodcaller
+
 import numpy as np
 import pandas as pd
 
 
-def link_data_and_events(events_file, data_file, limit=None, add_hours=None):
-    """ Creates data frame with events and linked.
+def process_events(events, data, limit=None, timezone=None):
+    """ Processes events and data CSV-files.
+
+        All events that have specified Time are linked with appropriate data.
+        Each all-day event description is saved into separate CSV-file.
 
         Arguments:
-            data_file (str): path CSV-file with ask/bid data
-            events_file (str): path to CSV-file with events
+            events (str): path CSV-file with ask/bid data
+            data (str): path to CSV-file with events
+
+        Returns:
+            linked_with_data (DataFrame):
+            several_days_events (DataFrame):
+    """
+    df_events = pd.read_csv(events, delimiter=';', index_col=False)
+    df_events = df_events[(df_events.Currency == "USD") | (df_events.Currency == "EUR")]
+    df_events = df_events[df_events.Importance == "H"]
+
+    df_data = pd.read_csv(data, index_col=False)
+
+    for text_column in ['Currency', 'Importance', 'Event']:
+        df_events[text_column] = df_events[text_column].str.strip()
+
+    indexer = df_events.Time.str.contains("\d\d:\d\d", regex=True, na=False)
+    timed_events, several_days_events = df_events[indexer], df_events[~indexer]
+
+    linked_with_data = link_data_and_events(timed_events, df_data, limit, timezone)
+
+    return linked_with_data, several_days_events
+
+
+def link_data_and_events(ev, data, limit=None, timezone=None):
+    """ Creates data frame with events and data linked.
+
+        Arguments:
+            ev (DateFrame): path CSV-file with ask/bid data
+            data (DataFrame): path to CSV-file with events
             limit (int): quantity of data rows to be processed
+            timezone (int): amount of hours to shift date columns
 
         Returns:
             DataFrame: pandas dataframe with events and data linked
     """
-    ev = pd.read_csv(events_file, delimiter=";")
-    data = pd.read_csv(data_file)
     if limit is not None:
         data = data.head(limit)
 
-    ev = ev[(ev.Currency == "USD") | (ev.Currency == "EUR")]
-    ev = ev[ev.Importance == "H"]
-    # ev = ev.drop(["Unnamed: " + str(i) for i in range(5, 10)], axis=1)
     date, time = ev["Date"], ev["Time"]
+    date_time_format = "%Y.%m.%d %H:%M"
+    timestamp_format = "%Y%m%d %H:%M:%S:%f"
 
-    ev["DateAndTime"] = [pd.to_datetime(d + " " + t, format="%d.%m.%Y %H:%M")
+    ev["DateAndTime"] = [pd.to_datetime(d + " " + t, format=date_time_format)
                          for d, t in zip(date, time)]
 
-    data["DateAndTime"] = [pd.to_datetime(ts, format="%Y%m%d %H:%M:%S:%f")
+    data["DateAndTime"] = [pd.to_datetime(ts, format=timestamp_format)
                            for ts in data["Timestamp"]]
 
-    if add_hours is not None:
-        ev.DateAndTime += datetime.timedelta(hours=add_hours)
-        data.DateAndTime += datetime.timedelta(hours=add_hours)
+    if timezone is not None:
+        ev.DateAndTime += timedelta(hours=timezone)
 
     output = list()
 
     # group events with proper data
     for event in ev.values:
         ts = event[-1]
-        one_min_before = ts - datetime.timedelta(minutes=1)
-        five_min_ahead = ts + datetime.timedelta(minutes=5)
+        one_min_before = ts - timedelta(minutes=1)
+        five_min_ahead = ts + timedelta(minutes=5)
         data_in_range = data[(data.DateAndTime >= one_min_before) &
                              (data.DateAndTime <= five_min_ahead)]
 
         for d in data_in_range.values:
             output.append(np.hstack((event[:-1], d)))
 
+    if not output:
+        return pd.DataFrame()
+
     cols = list(ev.columns[:-1]) + list(data.columns)
-    return pd.DataFrame(output, columns=cols)
+    dataframe = pd.DataFrame(output, columns=cols)
+
+    # synchronize original Date and Time columns with DateAndTime
+    dates = pd.Series(datetime.strptime(d + " " + t, date_time_format) + timedelta(hours=timezone)
+                      for d, t in zip(dataframe.Date, dataframe.Time))
+
+    dataframe["DateUTC"] = dates.map(methodcaller('date'))
+    dataframe["TimeUTC"] = dates.map(methodcaller('time'))
+
+    return dataframe
 
 
 if __name__ == "__main__":
-    df = link_data_and_events("HistoryEventsAll_Jan.csv", "EURUSD_Jan.csv",
-                              limit=500000, add_hours=7)
-    df.to_csv("linked.csv", index=False)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--events", type=str, help="path to events CSV-file")
+    parser.add_argument("-d", "--data", type=str, help="path to data CSV-file")
+    parser.add_argument("-l", "--limit", nargs='?', type=int, default=None,
+                        help="max records to be processed")
+    parser.add_argument("-t", "--timezone", nargs='?', type=int, default=7,
+                        help="date and time shift")
+    args = vars(parser.parse_args())
+
+    linked, special = process_events(**args)
+
+    if linked.empty:
+        raise ValueError("Error occurred: dataframe is empty")
+
+    linked.to_csv("linked.csv", index=False)
+    special.to_csv("special_events.csv", index=False)
--- eventsplotter.py
+++ eventsplotter.py
@@ -21,10 +21,14 @@ class EventsPlotter(QMainWindow):
 
     def __init__(self, size):
         super(EventsPlotter, self).__init__()
-        self.fileName = None
+        self.linkedFileName = None
+        self.specialFileName = None
         self.eventsData = None
+        self.specialEventsData = None
         self.plottedEvent = None
         self.eventCommentaries = defaultdict(lambda: "")
+
+        # GUI initialization
         self.createLayout()
         self.bindEvents()
         self.setMinimumSize(*size)
@@ -47,12 +51,12 @@ class EventsPlotter(QMainWindow):
 
         self.mainLayout.setRowStretch(0, 10)
         self.mainLayout.setColumnStretch(0, 10)
-        self.mainLayout.addWidget(self.plotWindow, 0, 0, 1, 3)
-        self.mainLayout.addWidget(self.btnPrevEvent, 1, 1)
-        self.mainLayout.addWidget(self.btnNextEvent, 1, 2)
-        self.mainLayout.addWidget(self.txtComment, 1, 0)
-        # self.mainLayout.addLayout(self.plotInfo, 0, 1, 1, 2)
-        # self.mainLayout.addLayout(self.plotInfo, 0, 1, 1, 2)
+        self.mainLayout.setRowMinimumHeight(0, 50)
+        self.mainLayout.addLayout(self.plotInfo, 0, 0, 1, 3)
+        self.mainLayout.addWidget(self.plotWindow, 1, 0, 1, 3)
+        self.mainLayout.addWidget(self.btnPrevEvent, 2, 1)
+        self.mainLayout.addWidget(self.btnNextEvent, 2, 2)
+        self.mainLayout.addWidget(self.txtComment, 2, 0)
 
         centralWidget.setLayout(self.mainLayout)
         self.setCentralWidget(centralWidget)
@@ -60,10 +64,15 @@ class EventsPlotter(QMainWindow):
 
     def createMenuBar(self):
         """ Adds menu bar to main window """
-        actionSelectFile = QAction("&Select CSV-file...", self)
+        actionSelectEventsFile = QAction("&Select CSV-file...", self)
+        # actionSelectSpecialEventsFile = QAction("&Select all day events...", self)
+
         optionsMenu = self.menuBar().addMenu("&Options")
-        optionsMenu.addAction(actionSelectFile)
-        actionSelectFile.triggered.connect(lambda: self.showChooseFileDialog())
+        optionsMenu.addAction(actionSelectEventsFile)
+        # optionsMenu.addAction(actionSelectSpecialEventsFile)
+
+        actionSelectEventsFile.triggered.connect(lambda: self.showChooseLinkedFileDialog())
+        # actionSelectSpecialEventsFile.triggered.connect(lambda: self.showChooseAllDayFileDialog())
 
     def bindEvents(self):
         self.btnPrevEvent.clicked.connect(self.plotPrevEvent)
@@ -73,16 +82,17 @@ class EventsPlotter(QMainWindow):
         """ Increments or decrements current event index and plots new graph onto canvas.
         """
         leavedEvent = self.getSameTimeEvents()
-        leavedEvent = self.CompoundEventSplitter.join(leavedEvent)
+        # step = len(leavedEvent)
+        joinedNames = self.CompoundEventSplitter.join(leavedEvent)
 
         if ahead:
             self.plottedEvent = min(self.plottedEvent + 1, len(self.eventNames) - 1)
         else:
             self.plottedEvent = max(0, self.plottedEvent - 1)
 
-        comment = self.txtComment.text()
+        comment = str(self.txtComment.text())
         if comment:
-            self.eventCommentaries[leavedEvent] = comment
+            self.eventCommentaries[joinedNames] = comment
 
         enteredEvent = self.getSameTimeEvents()
         enteredEvent = self.CompoundEventSplitter.join(enteredEvent)
@@ -100,27 +110,61 @@ class EventsPlotter(QMainWindow):
         """ Moves to previous plotted event """
         self.plotEvent(ahead=False)
 
-    def showChooseFileDialog(self):
-        """ Opens a file choosing dialog to select CSV-file with linked events info """
+    def prepareDialog(self):
+        """ Helper function for file choosing menu actions"""
         dlg = QFileDialog(self)
         dlg.setFileMode(QFileDialog.ExistingFile)
         dlg.setNameFilter("CSV-files (*.csv)")
+        return dlg
+
+    def showChooseLinkedFileDialog(self):
+        """ Opens a file choosing dialog to select CSV-file with linked events info """
+        dlg = self.prepareDialog()
         if dlg.exec():
-            self.fileName = dlg.selectedFiles()[0]
-            self.readCsv(delimiter=',')
-            self.plottedEvent = 0
+            self.linkedFileName = dlg.selectedFiles()[0]
+            self.eventsData = self.readCsv(self.linkedFileName, delimiter=',')
             self.eventNames = self.eventsData.Event.unique()
+            self.plottedEvent = 0
             self.plot()
             self.fillPlotInfo()
 
-    def readCsv(self, delimiter):
-        if self.fileName is None:
+    def showChooseAllDayFileDialog(self):
+        dlg = self.prepareDialog()
+        if dlg.exec():
+            self.specialFileName = dlg.selectedFiles()[0]
+            self.specialEventsData = self.readCsv(self.specialFileName, delimiter=',')
+
+    def readCsv(self, filename, delimiter):
+        if filename is None:
             return
 
-        # df = pd.read_csv(self.fileName, delimiter=delimiter)
-        # fmt = "%Y%m%d %H:%M:%S:%f"
-        # df["DateAndTime"] = [pd.to_datetime(ts, format=fmt) for ts in df.Timestamp]
-        self.eventsData = pd.read_csv(self.fileName, delimiter=delimiter)
+        df = pd.read_csv(filename, delimiter=delimiter)
+        return self.resolve_duplicates(df)
+
+    def resolve_duplicates(self, df):
+        """ Adds data postfixes to duplicated events names """
+        event_names = df.Event.unique()
+        processed = list()
+
+        for name in event_names:
+            dfslice = df[df.Event == name]
+            keys, groups = list(), list()
+
+            for k, g in dfslice.groupby(['DateUTC', 'TimeUTC']):
+                keys.append(k)
+                groups.append(g)
+
+            # if len(keys) > 1:
+            for (date, time), g in zip(keys, groups):
+                procdf = g
+                procdf["Event"] += " ({} {})".format(date, time)
+                processed.append(procdf)
+
+            # else:
+            #     processed.extend(groups)
+
+        newdf = pd.concat(processed)
+        return newdf
 
     def plot(self):
         """ Plots event graphs if events data was loaded.
@@ -129,6 +173,7 @@ class EventsPlotter(QMainWindow):
         """
         if self.eventsData is None or self.eventNames is None:
             return
+
         current_events = self.getSameTimeEvents()
         self.plotWindow.clearPlot()
         self.plotWindow.plot(self.eventsData, event_name=current_events,
@@ -140,44 +185,85 @@ class EventsPlotter(QMainWindow):
         df = self.eventsData
         event_name = self.eventNames[self.plottedEvent]
         current_event = df[df.Event == event_name]
-        dates, times = current_event.Date.unique(), current_event.Time.unique()
+        dates, times = current_event.DateUTC.unique(), current_event.TimeUTC.unique()
         date, time = dates[0], times[0]
 
-        same_time_events = df[(df.Date == date) & (df.Time == time)].Event
+        same_time_events = df[(df.DateUTC == date) & (df.TimeUTC == time)].Event
         return list(same_time_events.unique())
 
     def fillPlotInfo(self, ignore=None):
+        """
+        """
         if ignore is None:
-            ignore = ['Timestamp', 'DateAndTime', 'Event']
+            ignore = ['Timestamp', 'DateAndTime', 'Date', 'Time', 'Event']
 
         df = self.eventsData
-        current_event = df[df.Event == self.eventNames[self.plottedEvent]]
-        infos = [(k, v) for k, v in current_event.iloc[0].iteritems() if k not in ignore]
 
+        event_infos = dict()
+        for name in self.getSameTimeEvents():
+            current_event = df[df.Event == name]
+            date = current_event.Date.iloc[0]
+            event_infos[name] = [(k, v) for k, v in current_event.iloc[0].iteritems() if k not in ignore]
+
+        # remove widgets with info from previous event
         while True:
             widget_item = self.plotInfo.takeAt(0)
             if widget_item is None:
                 break
+            widget_item.widget().hide()
             self.plotInfo.removeWidget(widget_item.widget())
             widget_item.widget().deleteLater()
 
-        last = None
-        for row, (label, value) in enumerate(infos):
-            self.plotInfo.addWidget(QLabel(label + ":"), row, 0)
-            self.plotInfo.addWidget(QLabel(str(value)), row, 1)
-            last = row
+        last_row, info_length, info_labels = None, None, None
 
-        self.plotInfo.setRowStretch(last + 1, 10)
+        # dynamically fills info layout with widgets
+        for row, name in enumerate(event_infos):
+            labelEventName = QLabel(name)
+            labelEventName.setFont(QFont("Palatino", 10))
+            self.plotInfo.addWidget(labelEventName, row + 1, 0)
+            info = event_infos[name]
+
+            for col, (label, value) in enumerate(info, 1):
+                if row == 0:
+                    # first event processing so need add column headers
+                    self.plotInfo.addWidget(QLabel(label + ":"), 0, col)
+                self.plotInfo.addWidget(QLabel(str(value)), row + 1, col)
+
+            last_row = row
+            info_labels = info
+            info_length = len(info)
+
+        # TODO: finish with All Day events
+        # ev = self.specialEventsData
+        # if isinstance(ev, pd.DataFrame):
+        #     ev = ev[ev.Date == date]
+        #
+        #     if not ev.empty:
+        #         last_row += 1
+        #         self.plotInfo.addWidget(QLabel("Special events"), last_row, 0, info_length, 1)
+        #
+        #         labelEventName = QLabel(name)
+        #         labelEventName.setFont(QFont("Palatino", 10))
+        #         self.plotInfo.addWidget(labelEventName, last_row + 1, 0)
+        #
+        #         for col, (label, value) in enumerate(info, 1):
+        #             self.plotInfo.addWidget(QLabel(str(value)), row + 1, col)
+
+        self.plotInfo.setRowStretch(last_row + 1, 10)
+        self.repaint()
 
     def closeEvent(self, *args, **kwargs):
         """ Form closing event handler.
 
             Saves all commentraies that have been entered into CSV-file.
         """
-        if not self.fileName:
+        if not self.linkedFileName:
             return
 
-        with open(self.fileName.replace(".csv", "") + "_comments.csv", "w") as f:
+        if not any(self.eventCommentaries.values()):
+            return
+
+        with open(self.linkedFileName.replace(".csv", "") + "_comments.csv", "w") as f:
             w = csv.writer(f)
             w.writerow(['Event', 'Comment'])
             for event in self.eventCommentaries:
@@ -213,30 +299,21 @@ class PlotWindow(QDialog):
             event_name = event_name[0]
 
         ax = self.figure.add_subplot(1, 1, 1)
-        fed = df[df.Event == event_name]
-        fed = pd.DataFrame({"Ask price": fed["Ask price"].values,
-                            "Bid price": fed["Bid price"].values},
-                           #columns=["Ask price", "Bid price"],
-                           index=fed.DateAndTime)
 
         fed = df[df.Event == event_name]
         dates = fed.DateAndTime.values
-        # first, last = dates[0], dates[-1]
 
         # plot graph and setup plotting surface parametes
         ax.hold(True)
         ax = fed.plot(x=xcol, y=ycol, ax=ax, rot=20)
-        # data_range = pd.date_range(first, last, freq='30S').astype(datetime)
-        # ax.xaxis.set_ticks(data_range)
-        # ax.xaxis.set_ticklabels(data_range)
         ax.tick_params(axis='x', which='major', labelsize=9)
         ax.tick_params(axis='y', which='major', labelsize=10)
         yformatter = matplotlib.ticker.ScalarFormatter(useOffset=False)
         ax.yaxis.set_major_formatter(yformatter)
-        ax.set_title(concat_names if concat_names else event_name)
 
         # refresh canvas
         self.canvas.draw()
+        self.figure.tight_layout(pad=0.5)
 
 
 if __name__ == "__main__":
